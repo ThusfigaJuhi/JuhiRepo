854972021fd9c49012b5fb3eef0d673e3e249232
juhir <juhir@Juhi.mshome.net>
2025-05-05 15:17:02 +0530

    added new class for contact creation

---
+++ NewGitSample/src/test/java/crm/create/contact/CreateContactClass.java
@@ -0,0 +1,14 @@
+package crm.create.contact;
+
+public class CreateContactClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("contact is created");
+		
+		
+		
+
+	}
+
+}
