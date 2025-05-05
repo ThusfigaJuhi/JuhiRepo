ca0ce1f14468ec1970d315cc614f822c681d9b91
juhir <juhir@Juhi.mshome.net>
2025-05-05 15:06:09 +0530

    added another statement

--- NewGitSample/src/test/java/crm/create/contact/Sample.java
+++ NewGitSample/src/test/java/crm/create/contact/Sample.java
@@ -3,8 +3,13 @@ package crm.create.contact;
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("Hello world");
+		
+		System.out.println("hello Juhi");
+		
+		
+		
 	}
 
 }
